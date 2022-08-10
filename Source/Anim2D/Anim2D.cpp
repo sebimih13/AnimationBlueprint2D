e6feace6e5f7dbde50318b8646e1eb90bcecd3ef
sebi <sebimih@gmail.com>
2022-08-10 18:59:21 +0300

    Initial commit

---
+++ Source/Anim2D/Anim2D.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Anim2D.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Anim2D, "Anim2D" );
---
+++ Source/Anim2D/Anim2DGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Anim2DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIM2D_API AAnim2DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
